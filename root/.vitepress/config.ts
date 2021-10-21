7cb3711247ac09e0bcfc7551e1aee1e74717ff02
0x009922 <a.marcius26@gmail.com>
2021-10-21 20:55:17 +0300

    saving

--- root/.vitepress/config.ts
+++ root/.vitepress/config.ts
@@ -2,10 +2,39 @@ import { defineConfig } from 'vitepress'
 import Windi from 'vite-plugin-windicss'
 import path from 'path'
 
+interface SideBarItem {
+    text: string
+    link?: string
+    children?: SideBarItem[]
+}
+
+function getSidebarLinks(): SideBarItem[] {
+    return [
+        {
+            text: 'Главная',
+            link: '/',
+        },
+        {
+            text: 'Android',
+            children: [
+                {
+                    text: 'YouTube',
+                    link: '/android/youtube/',
+                },
+            ],
+        },
+    ]
+}
+
 export default defineConfig({
     base: '/grandma-sop',
     title: 'Стандартные операционные процедуры',
     vite: {
         plugins: [Windi({ config: path.resolve(__dirname, '../windi.config.ts') })],
+    },
+    themeConfig: {
+        sidebar: {
+            '/': getSidebarLinks(),
+        },
     },
 })
